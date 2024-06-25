37d2c2bd82aa1ef4930c595a5ee273d6f4eea6e7
Quester <quester561@gmail.com>
2024-06-25 00:27:16 -0700

    Revert "Update VehicleEnterExit.cs"
    This reverts commit f9be0db3a1aa15a3da334b59861cca21ef15b6ed.

--- Assets/scripts/VehicleEnterExit.cs
+++ Assets/scripts/VehicleEnterExit.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Hey there isnt everything OK
 public class VehicleEnterExit : MonoBehaviour
 {
     public string playerTag;
