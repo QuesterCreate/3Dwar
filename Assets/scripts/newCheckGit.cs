2658eec04e089346b040bf69dd35a876fea02407
Quester <quester561@gmail.com>
2024-06-24 22:23:11 -0700

    thrd status change

--- Assets/scripts/newCheckGit.cs
+++ Assets/scripts/newCheckGit.cs
@@ -1,2 +1,20 @@
-//Hey there 1st
-helsdfoasa svcns\svaosfof
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+sdfgsdfg
+public class PlayerManager : MonoBehaviour
+{
+    
+#region Singletion
+public static PlayerManager instance;
+
+void Awake(){
+
+instance=this;
+
+}
+
+#endregion
+public GameObject player;
+
+}
